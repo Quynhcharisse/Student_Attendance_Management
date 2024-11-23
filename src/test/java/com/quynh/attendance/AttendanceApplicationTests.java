4d41f3f3b85fe2e07f3cdf3e98cffed448f98ce0
QuynhCharisses <vannhuquynhp@gmail.com>
2024-11-23 12:54:53 +0700

    feat: add new project

---
+++ src/test/java/com/quynh/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quynh.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
